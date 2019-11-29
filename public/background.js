7d2c80abfe4716c859275dd524e25313ebd7cf03
Will Smith <lwilsonsmith@gmail.com>
2019-11-28 20:32:05 -0500

    Adds MVP for chrome extension Netflix Repeat One

---
+++ public/background.js
@@ -0,0 +1,33 @@
+function updateStorage(status) {
+  chrome.storage.sync.set({'status': status});
+}
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    id: 'Enable',
+    title: 'Enable',
+    type: 'radio',
+    contexts: ['all'],
+  }, updateStorage('Enabled'));
+  chrome.contextMenus.create({
+    id: 'Disable',
+    title: 'Disable',
+    type: 'radio',
+    contexts: ['all'],
+  });
+});
+
+chrome.contextMenus.onClicked.addListener(function(item, tab) {
+  updateStorage(item.menuItemId);
+  chrome.contextMenus.update(item.menuItemId, { checked: true })
+});
+
+chrome.storage.onChanged.addListener(function(changes) {
+  chrome.storage.sync.get("status", function (obj) {
+    chrome.tabs.query({}, tabs => {
+      tabs.forEach(tab => {
+        const tabId = tab.id;
+        chrome.tabs.sendMessage(tabId, obj)
+      });
+    });
+  });
+});
